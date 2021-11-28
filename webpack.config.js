dd98e1f50d49644b37e5894be9d896919aa92eb4
Mikhail Stankevich <Mikhail_Stankevich@epam.com>
2021-11-28 23:28:22 +0300

    feat: add webpack, jest, nodemon

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    server:'./server.js'
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  target: 'node',
+  node: {
+    __dirname: false,
+    __filename: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+      }
+    ]
+  },
+}
